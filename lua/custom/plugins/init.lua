c1eab72a4f327aae5c90bbed2143ea1a0378ff51
Krishna Bharadwaj <bharadwaj6598@gmail.com>
2025-11-06 19:59:09 -0800

    okay config

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,20 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
+-- Set tab-related options for 4 spaces
+vim.opt.tabstop = 4
+vim.opt.expandtab = true
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+
+-- Enable autoindentation
+vim.opt.autoindent = true
+vim.opt.smartindent = true
+vim.opt.termguicolors = true
+vim.keymap.set('n', '<C-f>', '<C-f>zz')
+vim.keymap.set('n', '<C-b>', '<C-b>zz')
+vim.keymap.set('n', '<C-d>', '<C-d>zz')
+vim.keymap.set('n', '<C-u>', '<C-u>zz')
+vim.keymap.set('n', 'n', 'nzzzv')
+vim.keymap.set('n', 'N', 'Nzzzv')
 return {}
